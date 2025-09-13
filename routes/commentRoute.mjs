2d0585355f8c90509eba7afa1f60d1ecb54140fb
Ismael Ludens Alexandre <alexandreludens2@gmail.com>
2025-09-12 23:55:37 -0400

    edit bugs in comments

--- routes/commentRoute.mjs
+++ routes/commentRoute.mjs
@@ -1,22 +1,22 @@
- import express from 'express';
- 
- import { comments, users } from '../utilities/database.mjs'
- 
- const router = express.Router();
- 
- 
- 
- router
-   .route("/")
-   .get((req, res) => {
-     // Generate HTML for all users
-     let commentsHtml = '';
-     comments.forEach((comment, index) => {
-        const user = users.find((user)=> user.id == comment.userId);
+import express from 'express';
 
-        // const usercomments = comment.userId == user.id
+import { comments, users } from '../utilities/database.mjs'
 
-       commentsHtml += `<div class="comment">
+const router = express.Router();
+
+
+
+router
+    .route("/")
+    .get((req, res) => {
+        // Generate HTML for all users
+        let commentsHtml = '';
+        comments.forEach((comment) => {
+            const user = users.find((user) => user.id == comment.userId);
+
+            // const usercomments = comment.userId == user.id
+
+            commentsHtml += `<div class="comment">
               <div class="comment-header">
                 <div class="comment-meta">
                   <span class="comment-author">${user.name}</span>
@@ -30,15 +30,15 @@
               </div>
             </div>
               `;
+        })
+        /*---------------------------------*/
+        const options = {
+            title: "MiniBlog - All comments",
+            content: comments.length,
+            commentsHtml: commentsHtml
+        };
+
+        res.render("comments", options);
     })
-    /*---------------------------------*/
-    const options = {
-      title: "MiniBlog - All comments",
-      content: comments.length,
-    commentsHtml: commentsHtml
-    };
 
-    res.render("comments", options);
-  })
-
-  export default router;
+export default router;
